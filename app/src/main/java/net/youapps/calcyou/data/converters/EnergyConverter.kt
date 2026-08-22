f7cd49b96eaf27986505edd132f4a724a8b1879c
Bnyro <bnyro@tutanota.com>
2026-08-22 11:49:03 +0200

    feat: add (kilo)watt hours energy units (closes #106)

--- app/src/main/java/net/youapps/calcyou/data/converters/EnergyConverter.kt
+++ app/src/main/java/net/youapps/calcyou/data/converters/EnergyConverter.kt
@@ -7,6 +7,8 @@ class EnergyConverter : UnitConverter<Double> {
         FactorUnit(R.string.joule, 1.0),
         FactorUnit(R.string.kilojoule, 1E3),
         FactorUnit(R.string.megajoule, 1E6),
+        FactorUnit(R.string.watt_hours, 60 * 60),
+        FactorUnit(R.string.kilowatt_hours, 1000 * 60 * 60),
         FactorUnit(R.string.calorie, 4.184),
         FactorUnit(R.string.kilocalorie, 4.184E3),
         FactorUnit(R.string.grams_of_carbohydrate, 4.0 * 4.184E3),
